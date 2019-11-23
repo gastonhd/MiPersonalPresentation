82a449150ffa607974fa8f44f5b909bf3bc97b72
gastonhd <gaston1.9974v@gmail.com>
2019-11-23 13:39:34 -0300

    integration whith firebase

--- app/src/main/java/com/gastondominguez/mipersonalpresentation/MainActivity.kt
+++ app/src/main/java/com/gastondominguez/mipersonalpresentation/MainActivity.kt
@@ -2,11 +2,48 @@ package com.gastondominguez.mipersonalpresentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.gastondominguez.mipersonalpresentation.models.User
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var db = FirebaseFirestore.getInstance()
+    private var id = ""
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            db.collection("users").get().addOnSuccessListener { result ->
+                for (document in result) {
+                    id = document.id
+                    val user = document.toObject(User::class.java)
+                    nameValue.text = user.name
+                    addressValue.text = user.adrress
+                    ageValue.text = user.age.toString()
+                    phoneValue.text = user.phone
+                    lastNameValue.text = user.lastName
+
+                }
+
+            }.addOnFailureListener {
+
+            }
+
+        }
+
+
+        saveInfo.setOnClickListener {
+            val washingtonRef = db.collection("users").document(id)
+            washingtonRef.update("sex", sexValue.text.toString()).addOnSuccessListener {
+                Log.d("update", "success")
+
+            }.addOnFailureListener { Log.d("update", "failed") }
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/gastondominguez/mipersonalpresentation/models/User.kt
@@ -0,0 +1,9 @@
+package com.gastondominguez.mipersonalpresentation.models
+
+data class User(
+    val name: String ="",
+    val lastName: String="",
+    val phone: String="",
+    val age: Int=0,
+    val adrress: String=""
+) {}
